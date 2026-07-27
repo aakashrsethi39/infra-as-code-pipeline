c7d558a7ae2819b7148737f7ca67d23200e742a8
Aakash Sethi <aakashrsethi39@gmail.com>
2026-07-27 17:18:19 +0530

    staging

--- app/app.py
+++ app/app.py
@@ -1,23 +1,14 @@
-# app.py
-
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello from ECS Fargate"
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 @app.route("/health")
 def health():
-    return {
-        "status": "healthy"
-    }, 200
-
+    return {"status": "healthy"}, 200
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=PORT
-    )
+    app.run(host="0.0.0.0", port=3000)
