3c5acac71799d9f7c79971a7cdbcdd37fcd1edbd
Aakash Sethi <aakashrsethi39@gmail.com>
2026-07-28 14:35:46 +0530

    Remove unused locals1

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -10,8 +10,3 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "project_name" {
-  description = "Project name used for resource naming"
-  type        = string
-  default     = "ecommerce"
-}
